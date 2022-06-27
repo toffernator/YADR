33bafcb0393584469593755116100fe5382ac006
Christoffer Gram <chkg@itu.dk>
2022-06-27 23:03:25 +0200

    All the Stuff. At least the first of all the stuff...

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check prequisites
+command -v stow >/dev/null 2>&1 || { echo >&2 "stow is not installed.  Aborting."; exit 1; }
+
+if [ -z $STOW_FOLDERS ]
+then
+	STOW_FOLDERS=alacritty,git,i3,picom
+fi
+
+# Create symlinks
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do
+	echo "Stowing $folder..."
+	stow -D $folder
+	stow $folder
+done
+
---
+++ programs/help.md
@@ -0,0 +1,3 @@
+# Install Packages from a List
+
+Read [this](https://wiki.archlinux.org/title/pacman/Tips_and_tricks#Install_packages_from_a_list) link
